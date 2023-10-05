52f40cce77851d3659a263deec708cc6eb941120
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-05 23:21:36 +0000

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,13 @@
 /**
  * main - Entry point
  *
- * Return: 0 (Success)
+ * Description: print a quote using write function
+ * write(int fd,const void *buf,size_t count)
+ *
+ * Return: 1 (not success).
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
