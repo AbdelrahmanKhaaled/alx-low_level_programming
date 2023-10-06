c857d415f6d99189d52ef750b676cda5d30c4e7c
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-06 19:39:25 +0000

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabitcs in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
