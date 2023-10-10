0c40355098ab7df2454082e6df7a1bc45cd23a2c
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-10 19:54:41 +0000

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include "main.h"
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *   Description: print all alphabitcs in lowercase
+ * Description: print all alphabitcs in lowercase
  *
- *   Return: 0 (Success)
- */
+ * Return: 0 (Success)
+*/
 
 int main(void)
 {
