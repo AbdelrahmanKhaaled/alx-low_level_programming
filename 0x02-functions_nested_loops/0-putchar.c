93e85cdb993cc808ca1e81591771e5eb924858a2
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-10 19:50:35 +0000

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *  main - Entry point
+ *
+ *   Description: print all alphabitcs in lowercase
+ *
+ *   Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabitcs in lowercase using print_alphabet
+ *
+ * Return: 0 (Success)
+*/
+
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
