6d0ffaf86990210cad688b4e6e8bf2252a15c91b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-11 06:00:03 +0000

    functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0 ; i <=2 ; i++)
+	for (i = 0 ; i <= 2 ; i++)
 		for (j = 0 ; j <= 3 ; j++)
 			for (k = 0 ; k <= 5 ; k++)
 				for (l = 0 ; l <= 9 ; l++)
@@ -19,7 +19,6 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar(k + '0');
 					_putchar(l + '0');
-					if((i + j + k + l) != 19)
-						_putchar('\n');
+					_putchar('\n');
 				}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,23 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, res;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar((i * j));
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			res = i * j;
+			if (res > 9)
+				_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0')
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (res <= 9)
+					_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
