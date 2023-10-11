f492cd2c2d02d58cadb788378ce57f13cde06bc5
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-11 04:26:10 +0000

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar((i * j) + '0');
+			_putchar((i * j));
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
