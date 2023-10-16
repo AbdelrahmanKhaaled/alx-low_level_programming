487933e6ddd860519bae458494f4859c3252ac85
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-16 21:31:24 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ *
+ * @str: checks input of function.
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *
+ * @s: checks input of function.
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 2; i >= 0 ; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
