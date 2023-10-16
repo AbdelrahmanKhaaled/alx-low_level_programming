cc40a25547d96351d4163054763ea930f97f2b0d
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-16 23:23:36 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *  
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if 'c' is alphabet
+ *     otherwise always 0 (Success)
+*/
+
+int _atoi(char *s)
+{
+	unsigned long int i;
+	int num = 0;
+	char ch = '+';
+	for (i = 0 ; i < strlen(s) ; i++)
+	{
+		if (s[i] == '-' && ch == '-')
+			ch = '+';
+		else if (s[i] == '+' && ch == '-')
+			ch = '-';
+		else if (s[i] == '-' && ch == '+')
+			ch = '-';
+		else if (s[i] == '+' && ch == '+')
+			ch = '+';
+		else if (s[i] >= '0' && s[i] <= '9')
+			num = (num * 10) + (int) (s[i]) ;
+
+	}
+	if(ch = '+')
+		return (num);
+	else
+		return (-num);
+}
