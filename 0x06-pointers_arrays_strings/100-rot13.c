5d387c9ea1e1c04f1fdbd650ad97d8d12f6495e2
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-19 23:50:57 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,27 +11,22 @@
 
 char *rot13(char *s)
 {
-	unsigned long int i;
-	int j;
+	int i;
 	char *c = s;
-	char arr[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-			'M',
-			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char arr2[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-			'Z',
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
-
-	for (i = 0 ; i < strlen(s) ; i++)
+	char arr[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arr2[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	while (*s)
 	{
-		for (j = 0 ; j < 52 ; j++)
+		for (i = 0 ; i <= 52 ; i++)
 		{
-			if (s[i] == arr[j])
-				s[i] = arr2[j] + '0';
+			if (*s == arr[j])
+			{
+				*s = arr2[j] + '0';
+				break;
+			}
 		}
+		s++;
 	}
 	return (c);
 }
