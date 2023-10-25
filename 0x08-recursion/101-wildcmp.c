1ab764acaacad070010eea82712f5e7ec7aa0ca8
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-25 23:16:49 +0000

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,7 +22,7 @@ int wildcmp(char *s1, char *s2)
 	else if (s2[0] == '*' && s1[0] != s2[1])
 		return (wildcmp(++s1, s2));
 	else if (s2[0] == '*' && s1[0] == s2[1])
-		return (wildcmp(s1, ++s2));
+		return (wildcmp(++s1, s2));
 	else
 		return (0);
 }
