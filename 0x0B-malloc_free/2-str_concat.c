c9a5b0005ab84b78c655f938b6ff4bbaddceb909
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-10-31 23:21:19 +0000

    malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,15 +16,16 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "\0";
+		*s1 = "\0";
 	}
 	if (s2 == NULL)
 	{
-		s2 = "\0";
+		*s2 = "\0";
 	}
 	else
 	{
 		m = malloc((strlen(s1) + strlen(s2)) * sizeof(char) + 1);
+
 		if (m == NULL)
 		{
 			return (NULL);
