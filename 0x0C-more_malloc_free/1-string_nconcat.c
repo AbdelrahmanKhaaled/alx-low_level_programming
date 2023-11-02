b3292d42c240639c1587ed3bacf03f110d67562b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-02 21:06:38 +0000

    More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,11 +20,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	for (len_s1 = 0 ; s1[len_s1] != '\0' ; len_s1++)
-
+		continue;
 	str = malloc(len_s1 + n + 1);
 	if (str == NULL)
 		return (NULL);
-	
+
 	for (i = 0 ; s1[i] != '\0' ; i++)
 	{
 		str[i] = s1[i];
