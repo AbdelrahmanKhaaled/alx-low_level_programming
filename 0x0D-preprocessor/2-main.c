d6a03940165b3da695cf9625b3f8b7130f84bfda
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-06 22:25:22 +0000

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabitcs in lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
