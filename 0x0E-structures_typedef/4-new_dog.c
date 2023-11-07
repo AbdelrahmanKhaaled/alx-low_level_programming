8d5211ffe7351ab4a03667dae075458b1453a7e5
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-07 00:56:48 +0000

    Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,26 @@
 #include <stdlib.h>
 
 /**
+ * _strcpy - copy string src into string dest.
+ *
+ * @dest: checks input of function
+ * @src: checks input of function
+ *
+ * Return: returns 'dest'.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog.
  *
  * @name: checks input of function
@@ -37,9 +57,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = name;
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
