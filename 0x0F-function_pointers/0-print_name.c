999ba12552600a2a887fa975cc69ba388a171d0b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-08 21:53:31 +0000

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -13,6 +13,33 @@ void print_name_as_is(char *name)
 }
 
 /**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+*/
+
+void print_name_uppercase(char *name)
+{
+	unsigned int i;
+	
+	printf("Hello, my uppercase name is ");
+	i = 0;
+	while (name[i])
+	{
+		if (name[i] >= 'a' && name[i] <= 'z')
+		{
+			putchar(name[i] + 'A' - 'a');
+		}
+		else
+		{
+			putchar(name[i]);
+		}
+		i++;
+	}
+}
+
+/**
  * print_name - prints a name.
  *
  * @name: checks input of function
