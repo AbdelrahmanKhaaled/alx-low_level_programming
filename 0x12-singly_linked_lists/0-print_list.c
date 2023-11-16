fea9501c02c20a92c46ac6ffc92e7cd0529d05b7
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-16 20:26:41 +0000

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * _strlen - number of characters.
+ *
+ * @s: checks input of function
+ *
+ * Return: returns number of characters
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		count++;
+	return (count);
+}
+
+/**
  * print_list - prints all the elements of a list_t list.
  *
  * @h: checks input of function
