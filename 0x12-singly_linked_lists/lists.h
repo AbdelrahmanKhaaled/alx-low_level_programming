12d58bc9539264fd990036d237fea7bc0294d32e
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-16 19:52:18 +0000

    Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,13 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/*
- * struct list_s - structer of list.
+/**
+ * struct list_s - single linked list.
  *
  * @str: string in each node
- * @str: len of his string in each node
+ * @len: len of his string in each node
  * @next: next pointer in each node
+ *
+ * Description: single linked list.
 */
 
 typedef struct list_s
@@ -19,6 +22,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
