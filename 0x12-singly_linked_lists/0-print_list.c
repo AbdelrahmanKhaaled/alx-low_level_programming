19f4a2b0744078c9c888ba3474aeb9d897aaf9f9
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-16 19:33:18 +0000

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 
 	while (current != NULL)
 	{
-		printf("[%d] %s\n", current -> len, current -> str);
+		printf("[%u] %s\n", current -> len, current -> str);
 		current = current -> next;
 		count++;
 	}
