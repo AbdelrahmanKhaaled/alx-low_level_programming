a28cdab1eba3146304781b2319e722d7426f1c83
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-24 18:41:02 +0000

    Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - returns the value of a bit at a given index.
- * 
+ *
  * @n: checks input of function
  * @index: checks input of function
  *
@@ -12,10 +12,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int length;
-	
+
 	length = 0;
 	while (n)
-	{		
+	{
 		if (length == index)
 			return (n % 2);
 		n = n / 2;
