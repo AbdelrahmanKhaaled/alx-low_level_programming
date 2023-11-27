25bcc8b7fda3f2fc56d59357fb68119fbf9d2815
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-27 19:47:59 +0000

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output..
+ *
+ * @filename: checks input of function
+ * @letters: checks input of function
+ *
+ * Return: returns the actual number of letters it could read and print.
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buffer[50];
+	char buffer[1024 * 8];
 
 	fd = open(filename, O_RDONLY);
 	if (!filename || !letters || fd == -1)
 		return (0);
 	bytes = read(fd, &buffer[0], letters);
 	bytes = write(STDOUT_FILENO, &buffer[0], bytes);
+	close(fd);
 
 	return (bytes);
 }
