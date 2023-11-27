e0b264ec93bc93301eb2f035c63a24bf3a0857e8
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-27 20:10:03 +0000

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes;
-	char buffer[1024 * 6];
 
-	if (!text_content)
+	if (!filename || !text_content)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (write(fd, text_content, strlen(text_content)) == -1)
+	bytes = write(fd, text_content, strlen(text_content));
+	if (bytes == -1)
 		return (-1);
 	close(fd);
 
