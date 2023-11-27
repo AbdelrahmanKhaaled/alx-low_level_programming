614d9bac63df7014ceb670c19ac8b76c91ce618b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2023-11-27 19:49:58 +0000

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,9 +15,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t bytes;
 	char buffer[1024 * 8];
-
+	
+	if (!filename || !letters)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (!filename || !letters || fd == -1)
+	if (fd == -1)
 		return (0);
 	bytes = read(fd, &buffer[0], letters);
 	bytes = write(STDOUT_FILENO, &buffer[0], bytes);
